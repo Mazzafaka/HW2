26f772532396ae7250b5bfb5c6f17a27cd75f424
Zack <1984odina.gmail.com>
2023-07-09 23:45:58 +0600

    Added 1

---
+++ EX1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// int NewRandom(){
+//     int number = new Random().Next(100, 1000);
+//     return number;
+// }
+// int GetSecondDigit(int random)
+//     {
+//         return (random / 10) % 10;
+//     }
+// int random = NewRandom();
+// int mNumber = GetSecondDigit(random);
+// Console.WriteLine($"{random} -> {mNumber}");
