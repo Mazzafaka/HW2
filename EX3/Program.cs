3291f30b67c4d7a040a505dff86f85ecfe330783
Zack <1984odina.gmail.com>
2023-07-09 23:46:15 +0600

    Added 3

---
+++ EX3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+    bool IsWeekend(int dayOfWeek)
+    {
+        return dayOfWeek == 6 || dayOfWeek == 7;
+    }
+    Console.Write("Введите цифру, обозначающую день недели: ");
+    int dayOfWeek = int.Parse(Console.ReadLine());
+    bool isWeekend = IsWeekend(dayOfWeek);
+
+    if (isWeekend){
+        Console.WriteLine("День является выходным.");
+    }
+    else{
+        Console.WriteLine("День не является выходным.");
+    }
